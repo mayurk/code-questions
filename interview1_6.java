245430f6c1f9b35172e6deeefbdbf002a7866fef
Mayur Kharkar <mayur.kharkar@gmail.com>
2017-05-21 01:02:07 -0400

    In place rotation

--- interview1_6.java
+++ interview1_6.java
@@ -10,13 +10,14 @@ public class interview1_6 {
 		int data = 1;
 		for (int i=0; i<size; i++) {
 			for (int j=0; j < size; j++) {
-				System.out.print(data);
+				System.out.format("%3d", data);
 				image[i][j] = data++;
 			}
 			System.out.println("");
 		}
 
-		rotateRight(image);
+		//rotateRight(image);
+		rotateRightInPlace(image);
 		
 	}
 	public static void rotateRight(int[][] image) {
@@ -36,6 +37,46 @@ public class interview1_6 {
 			System.out.println("");
 		}
 
+	}
+
+	public static void rotateRightInPlace(int[][] image) {
+		int size = image[0].length;
+		System.out.println("size : " + size);
+
+		int k = 1;
+		int rowCnt = (size % 2 == 0) ? size /2 : size /2 - 1;
+		for (int i=0; i<size/2; i++) {
+			for (int j=i; j < size - k; j++) {
+				image = rotRight(image, i, j);
+//				for (int m=0; m<size; m++) {
+//					for (int n=0; j < size; n++) {
+//						System.out.print(image[m][n]);
+//					}
+//					System.out.println("");
+//				}
+			}
+			k++;
+		}
+
+		for (int i=0; i<size; i++) {
+			for (int j=0; j < size; j++) {
+				System.out.format("%3d", image[i][j]);
+			}
+			System.out.println("");
+		}
+
+	}
+
+	private static int[][] rotRight(int[][] image, int i, int j) {
+		int size = image[0].length;
+		int pixel = image[j][size-i-1];
+		image[j][size-i-1] = image[i][j];
+		int pixel2 = image[size-i-1][size-j-1];
+		image[size-i-1][size-j-1] = pixel;
+		int pixel3 = image[size-j-1][size-(size-i-1)-1];
+		image[size-j-1][size-(size-i-1)-1] = pixel2;
+		image[size-(size-i-1)-1][size-(size-j-1)-1] = pixel3;
+		return image;
 	}
 
 }
